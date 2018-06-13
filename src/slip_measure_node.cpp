91f03b6ecb2511e2f72b69d45558fa657e18c884
Przemek Muszynski <auerius92@gmail.com>
2018-06-13 18:56:01 +0200

    Slip estimation fixed

--- src/slip_measure_node.cpp
+++ src/slip_measure_node.cpp
@@ -6,10 +6,10 @@
 #include <math.h>
 
 /* Global variables */
-geometry_msgs::Vector3 forces[4]; // Storage for actual every leg forces
 geometry_msgs::Vector3 act_slip[4]; // Storage for every slip in legs
-geometry_msgs::Vector3 temps[4]; // Storage for old positions of legs
-bool legOnGround[4]; // Storage for actual states of legs - true: on the ground, false: in the air
+geometry_msgs::Vector3 on_ground_pos[4]; // Storage for old positions of legs
+geometry_msgs::Vector3 act_pos[4];
+
 ros::Publisher pubLeg0,pubLeg1,pubLeg2,pubLeg3;
 /* Function for calculating length of force vector */
 float forceDist(geometry_msgs::Vector3 f){ 
@@ -17,89 +17,110 @@ float forceDist(geometry_msgs::Vector3 f){
 }
 /* Function callback for position of legs topic */
 void positionCallback(const visualization_msgs::MarkerArray& msg){
-    for (int i = 0;i<4;i++){ // Go through every leg of robot
-        if (legOnGround[i]){
-            // Sum previous value of slip and calculate next until Leg is on the ground
-            act_slip[i].x += std::abs(msg.markers[i].pose.position.x - temps[i].x);
-            act_slip[i].y += std::abs(msg.markers[i].pose.position.y - temps[i].y);
-            act_slip[i].z += std::abs(msg.markers[i].pose.position.z - temps[i].z);
-        }
-        else
-        {
-            // Reset values of act_slip - Leg is in the air
-            act_slip[i].x = 0.0f;
-            act_slip[i].y = 0.0f;
-            act_slip[i].z = 0.0f;                
-        }
-        // Save old position of legs
-        temps[i].x = msg.markers[i].pose.position.x; 
-        temps[i].y = msg.markers[i].pose.position.y; 
-        temps[i].z = msg.markers[i].pose.position.z;   
+    for (int i = 0;i<4;i++){
+        act_pos[i].x = msg.markers[i].pose.position.x;
+        act_pos[i].y = msg.markers[i].pose.position.y;
+        act_pos[i].z = msg.markers[i].pose.position.z;        
     }
-    // Publish calculated actual slips to seperate topics
-    pubLeg0.publish(act_slip[0]);
-    pubLeg1.publish(act_slip[1]);
-    pubLeg2.publish(act_slip[2]);
-    pubLeg3.publish(act_slip[3]);
-} //Leg count: 0 - LF,1 - RF,2 - LH,3 -RH
+} // Feet count: 0 - LF,1 - RF,2 - LH,3 -RH
 /* Function callback for leg 0 forces topic (very similiar four callbacks) */
 void forceLFCallback(const geometry_msgs::WrenchStamped& msg){
-    // Store on callback values of leg forces
-    forces[0].x = msg.wrench.force.x;
-    forces[0].y = msg.wrench.force.y;
-    forces[0].z = msg.wrench.force.z;
-    // Check if force is very small; if true: leg is in the air
-    if (forceDist(forces[0])<0.01){
-        legOnGround[0] = false;            
+    static bool toggleState = false;
+    // Check if force is very small; if true: foot is in the air
+    if (forceDist(msg.wrench.force)<0.01){
+        // foot is in the air
+        if (toggleState==true)
+        {
+            toggleState = false;
+            act_slip[0].x = std::abs(on_ground_pos[0].x-act_pos[0].x);
+            act_slip[0].y = std::abs(on_ground_pos[0].y-act_pos[0].y);
+            act_slip[0].z = std::abs(on_ground_pos[0].z-act_pos[0].z);
+            pubLeg0.publish(act_slip[0]);
+        }
     }
     else{
-        legOnGround[0] = true;       
+        if (toggleState==false){
+            toggleState = true;
+            on_ground_pos[0].x = act_pos[0].x;
+            on_ground_pos[0].y = act_pos[0].y;
+            on_ground_pos[0].z = act_pos[0].z;
+        }             
     }
 }
 /* Function callback for leg 1 forces topic */
 void forceRFCallback(const geometry_msgs::WrenchStamped& msg){
-    forces[1].x = msg.wrench.force.x;
-    forces[1].y = msg.wrench.force.y;
-    forces[1].z = msg.wrench.force.z;
-    if (forceDist(forces[1])<0.01){
-        legOnGround[1] = false;            
+    static bool toggleState = false;
+    // Check if force is very small; if true: foot is in the air
+    if (forceDist(msg.wrench.force)<0.01){
+        // foot is in the air
+        if (toggleState==true)
+        {
+            toggleState = false;
+            act_slip[1].x = std::abs(on_ground_pos[1].x-act_pos[1].x);
+            act_slip[1].y = std::abs(on_ground_pos[1].y-act_pos[1].y);
+            act_slip[1].z = std::abs(on_ground_pos[1].z-act_pos[1].z);
+            pubLeg1.publish(act_slip[1]);
+        }
     }
     else{
-        legOnGround[1] = true;       
-    }    
+        if (toggleState==false){
+            toggleState = true;
+            on_ground_pos[1].x = act_pos[1].x;
+            on_ground_pos[1].y = act_pos[1].y;
+            on_ground_pos[1].z = act_pos[1].z;
+        }             
+    }   
 }
 /* Function callback for leg 2 forces topic */
 void forceLHCallback(const geometry_msgs::WrenchStamped& msg){
-    forces[2].x = msg.wrench.force.x;
-    forces[2].y = msg.wrench.force.y;
-    forces[2].z = msg.wrench.force.z;
-    if (forceDist(forces[2])<0.01){
-        legOnGround[2] = false;            
+    static bool toggleState = false;
+    // Check if force is very small; if true: foot is in the air
+    if (forceDist(msg.wrench.force)<0.01){
+        // foot is in the air
+        if (toggleState==true)
+        {
+            toggleState = false;
+            act_slip[2].x = std::abs(on_ground_pos[2].x-act_pos[2].x);
+            act_slip[2].y = std::abs(on_ground_pos[2].y-act_pos[2].y);
+            act_slip[2].z = std::abs(on_ground_pos[2].z-act_pos[2].z);
+            pubLeg2.publish(act_slip[2]);
+        }
     }
     else{
-        legOnGround[2] = true;       
+        if (toggleState==false){
+            toggleState = true;
+            on_ground_pos[2].x = act_pos[2].x;
+            on_ground_pos[2].y = act_pos[2].y;
+            on_ground_pos[2].z = act_pos[2].z;
+        }             
     }    
 }
 /* Function callback for leg 3 forces topic */
 void forceRHCallback(const geometry_msgs::WrenchStamped& msg){
-    forces[3].x = msg.wrench.force.x;
-    forces[3].y = msg.wrench.force.y;
-    forces[3].z = msg.wrench.force.z;
-    if (forceDist(forces[3])<0.01){
-        legOnGround[3] = false;            
+    static bool toggleState = false;
+    // Check if force is very small; if true: foot is in the air
+    if (forceDist(msg.wrench.force)<0.01){
+        // foot is in the air
+        if (toggleState==true)
+        {
+            toggleState = false;
+            act_slip[3].x = std::abs(on_ground_pos[3].x-act_pos[3].x);
+            act_slip[3].y = std::abs(on_ground_pos[3].y-act_pos[3].y);
+            act_slip[3].z = std::abs(on_ground_pos[3].z-act_pos[3].z);
+            pubLeg3.publish(act_slip[3]);
+        }
     }
     else{
-        legOnGround[3] = true;       
+        if (toggleState==false){
+            toggleState = true;
+            on_ground_pos[3].x = act_pos[3].x;
+            on_ground_pos[3].y = act_pos[3].y;
+            on_ground_pos[3].z = act_pos[3].z;
+        }             
     }    
 }
 /* Main loop of node */
 int main(int argc, char **argv){
-    // Initialization of data - reset every slip values
-    for (int i = 0;i<4;i++){
-        act_slip[i].x = 0.0f;
-        act_slip[i].y = 0.0f;
-        act_slip[i].z = 0.0f;    
-    }
     // Initialize node
     ros::init(argc,argv,"slip_measure");
     // Create node handler
